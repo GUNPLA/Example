b391ed7b171fa8dac9510995bdd5976d3eddfbb0
GUNPLA <LinuxExt4@gmail.com>
2014-03-23 01:08:19 +0800

    3.12

--- Example/src/Exs.cpp
+++ Example/src/Exs.cpp
@@ -6,26 +6,27 @@
  */
 
 #include<iostream>
-#include<iomanip>
-#include<cmath>
-
+#include<stdio.h>
 using namespace std;
 
 int main() {
-	float r, h, x1, x2, x3, x4, x5;
-	const float pi = 3.14;
-	cin >> r >> h;
-	x1 = 2 * pi * r;
-	x2 = pi * pow(r, 2);
-	x3 = 4 * pi * pow(r, 2);
-	x4 = 4 / 3 * pi * pow(r, 3);
-	x5 = x2 * h;
-	cout << setiosflags(ios::fixed) << setprecision(2);
-	cout << "圆周长为" << x1 << endl;
-	cout << "圆面积为" << x2 << endl;
-	cout << "圆球表面积为" << x3 << endl;
-	cout << "圆球体积为" << x4 << endl;
-	cout << "圆柱体积为" << x5 << endl;
+	int i, n;
+	char x[6];
+
+	for (i = 0; i < 6; i++) {
+
+		if ((x[i] = getchar()) == '\n')
+			break;
+	}
+	if (i == 6 || i == 0)
+		cout << "error";
+	else {
+		cout << "i=" << i << endl;
+		for (n = 0; n < i; n++)
+			cout << x[n] << endl;
+		for (n = i - 1; n >= 0; n--)
+			cout << x[n];
+	}
+
 	return 0;
 }
-
