e86043f5029ac463bb2806befb636046d18eb4b1
GUNPLA <LinuxExt4@gmail.com>
2014-04-15 15:31:33 +0800

    example 5.3

--- Example/src/Exs.cpp
+++ Example/src/Exs.cpp
@@ -9,19 +9,21 @@
 
 using namespace std;
 
-int a, m;
-
 int main() {
-	int b, x;
-	double y;
+	int a[10];
+	for (int i = 0; i < 10; i++) {
+		cin >> a[i];
+	}
 
-	extern double power();
-	b = 4;
-	cin >> a >> m;
-	x = a * b;
-	y = power();
-
-	cout << x << ',' << y << endl;
-
+	for (int i = 9; i > 0; i--) {
+		for (int j = 0; j < i; j++)
+			if (a[j] > a[j + 1]) {
+				a[j + 1] = a[j] + a[j + 1];
+				a[j] = a[j + 1] - a[j];
+				a[j + 1] = a[j + 1] - a[j];
+			}
+	}
+	for (int i = 0; i < 10; i++)
+		cout << a[i] << " ";
 	return 0;
 }
