eb6e4752b5e4b6ba459dda17c26577886b543630
GUNPLA <LinuxExt4@gmail.com>
2014-04-21 22:51:31 +0800

    Revert "5.5"
    This reverts commit ac394883952860d5aec7dce20f95995f744d49a0.

--- Example/src/Exs.cpp
+++ Example/src/Exs.cpp
@@ -10,25 +10,20 @@
 using namespace std;
 
 int main() {
-	int max_value(int x, int max);
-	int i, j, row = 0, colum = 0, max;
-	int a[3][4] = { { 5, 12, 23, 56 }, { 19, 28, 37, 46 }, { -12, -34, 6, 8 } };
-	max = a[0][0];
-	for (i = 0; i <= 2; i++)
-		for (j = 0; j <= 3; j++) {
-			max = max_value(a[i][j], max);
-			if (max == a[i][j]) {
-				row = i;
-				colum = j;
-			}
-		}
-	cout << "max=" << max << ",row=" << row << ",colum=" << colum << endl;
+	int a[10];
+	for (int i = 0; i < 10; i++) {
+		cin >> a[i];
+	}
 
+	for (int i = 9; i > 0; i--) {
+		for (int j = 0; j < i; j++)
+			if (a[j] > a[j + 1]) {
+				a[j + 1] = a[j] + a[j + 1];
+				a[j] = a[j + 1] - a[j];
+				a[j + 1] = a[j + 1] - a[j];
+			}
+	}
+	for (int i = 0; i < 10; i++)
+		cout << a[i] << " ";
 	return 0;
-}
-int max_value(int x, int max) {
-	if (x > max)
-		return x;
-	else
-		return max;
 }
