e5c504e1064d7d0c7babc98abce9c9ad4d1eb491
GUNPLA <LinuxExt4@gmail.com>
2014-04-24 23:27:10 +0800

    Call Windows Cmd

--- Example/src/Exs.cpp
+++ Example/src/Exs.cpp
@@ -6,35 +6,13 @@
  */
 
 #include<iostream>
+#include<stdlib.h>
 
 using namespace std;
 
-int main() {
-	void select_sort(int array[], int n);
-	int a[10], i;
-	cout << "enter the original array:" << endl;
-	for (i = 0; i < 10; i++)
-		cin >> a[i];
-	cout << endl;
-	select_sort(a, 10);
-	cout << "the sorted array:" << endl;
-	for (i = 0; i < 10; i++)
-		cout << a[i] << " ";
-	cout << endl;
+int main()
+{
+	system("dir");
+	system("pause");
 	return 0;
-}
-void select_sort(int array[], int n) {
-	int min, position;
-	for (int i = 0; i < n - 1; i++) {
-		min = array[i];
-		position = i;
-		for (int j = i + 1; j < n; j++) {
-			if (min > array[j]) {
-				min = array[j];
-				position = j;
-			}
-		}
-		array[position] = array[i];
-		array[i] = min;
-	}
 }
