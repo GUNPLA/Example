6552a5f94116c5aaa363271d83b284299d105a40
GUNPLA <LinuxExt4@gmail.com>
2014-04-28 23:47:04 +0800

    5.10

--- Example/src/Exs.cpp
+++ Example/src/Exs.cpp
@@ -6,13 +6,26 @@
  */
 
 #include<iostream>
-#include<stdlib.h>
+#include<cstring>
 
 using namespace std;
 
-int main()
-{
-	system("dir");
-	system("pause");
+int main() {
+	void max_string(char str[][30], int i);
+	int i;
+	char country_name[3][30];
+	for (i = 0; i < 3; i++) {
+		cin >> country_name[i];
+	}
+	max_string(country_name, 3);
 	return 0;
+}
+void max_string(char str[][30], int n) {
+	int i;
+	char string[30];
+	strcpy(string, str[0]);
+	for (i = 1; i < n; i++)
+		if (strcmp(str[i], string) > 0)
+			strcpy(string, str[i]);
+	cout << endl << "the largest string is:" << string << endl;
 }
