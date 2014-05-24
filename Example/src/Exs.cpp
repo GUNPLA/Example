d8c57929e8386b2e1097a70dc81ff1a031b2f986
GUNPLA <LinuxExt4@gmail.com>
2014-05-25 01:31:46 +0800

    5.1

--- Example/src/Exs.cpp
+++ Example/src/Exs.cpp
@@ -6,26 +6,26 @@
  */
 
 #include<iostream>
-#include<cstring>
 
 using namespace std;
-
 int main() {
-	void max_string(char str[][30], int i);
-	int i;
-	char country_name[3][30];
-	for (i = 0; i < 3; i++) {
-		cin >> country_name[i];
+	int a[10];
+	for (int i = 0; i < 10; i++) {
+		cin >> a[i];
 	}
-	max_string(country_name, 3);
+	for (int i = 0; i < 10; i++) {
+		int j = i, k;
+		for (int n = i + 1; n < 10; n++) {
+			if (a[j] > a[n])
+				j = n;
+		}
+		k = a[i];
+		a[i] = a[j];
+		a[j] = k;
+	}
+
+	for (int i = 0; i < 10; i++) {
+		cout << a[i] << ' ';
+	}
 	return 0;
-}
-void max_string(char str[][30], int n) {
-	int i;
-	char string[30];
-	strcpy(string, str[0]);
-	for (i = 1; i < n; i++)
-		if (strcmp(str[i], string) > 0)
-			strcpy(string, str[i]);
-	cout << endl << "the largest string is:" << string << endl;
 }
