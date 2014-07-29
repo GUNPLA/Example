f2090a062c8c28ccbd945995570c7de0f6ebd7e2
GUNPLA <LinuxExt4@gmail.com>
2014-07-29 20:25:25 +0800

    2014.7.29

--- Example/src/Exs.cpp
+++ Example/src/Exs.cpp
@@ -5,20 +5,22 @@
  *      Author: MAITO
  */
 
-#include<iostream>
-#include<cmath>
+//#include<iostream>
+#include<stdio.h>
 
 using namespace std;
 
 int main() {
-	for (int i = 2; i <= 100; i++) {
-		int j;
-		for (j = 2; j <= sqrt(i); j++)
-			if (i % j == 0) {
-				break;
-			}
-		if (j > sqrt(i))
-			cout << i << endl;
-	}
-	return 0;
+//	int a[3][4] = { 1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23 };
+//
+//	int *p;
+//	p = a[0];
+//	//for (p = a[0]; p < a[0] + 12; p++)
+//	cout << *p << ',' << *a[0]<<','<<*a;
+//	cout << endl;
+//	return 0;
+	int x;
+	int y=(1 && x);
+	printf("%d", x == y);
+//	return 0;
 }
